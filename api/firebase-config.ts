51ad42ddee9e726608241a004923409f0d46ccb3
WinnerSamurai6064 <ikegbuneagwu@gmail.com>
2026-05-30 20:48:49 +0100

    Remove Firebase config API route

--- api/firebase-config.ts
+++
@@ -1,6 +0,0 @@
-import firebaseConfig from '../firebase-applet-config.json';
-
-export default function handler(_req: any, res: any) {
-  res.setHeader('Cache-Control', 's-maxage=3600, stale-while-revalidate=86400');
-  res.status(200).json(firebaseConfig);
-}
